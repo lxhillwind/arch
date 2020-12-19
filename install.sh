45ac98f1207bbe2c85e62093b4ae86245704ad44
lxhillwind <lxhillwind@gmail.com>
2020-12-19 20:16:50 +0800

    print help in reliable way

--- install.sh
+++ install.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env sh
 
-# # Intro
-# Install archlinux as a guest (qemu / VirtualBox ...) with a script.
-#
-# **WARNING: /dev/sda (and /dev/sdb if it exists) will be eraised! (if `env
-# INSTALL=1` is passed in)**
-#
-# When /dev/sdb is available, /dev/sda (/dev/sda1) will be mounted as /boot,
-# and /dev/sdb will be used as super block (no partition, easier to resize).
-#
-# # Usage
-# In archlinux iso session, download this script; then:
-#
-# ```sh
-# # without INSTALL=1, help message will be printed.
-# INSTALL=1 sh ./install.sh
-# ```
-#
-# # NOTE
-# - username (sudo-ed) / password / hostname: all are `box`.
-# - timezone is set to Asia/Shanghai.
-
 {  # ensure whole script is loaded.
-
 set -e
 
+HELP='# Intro
+Install archlinux as a guest (qemu / VirtualBox ...) with a script.
+
+**WARNING: /dev/sda (and /dev/sdb if it exists) will be eraised! (if `env
+INSTALL=1` is passed in)**
+
+When /dev/sdb is available, /dev/sda (/dev/sda1) will be mounted as /boot,
+and /dev/sdb will be used as super block (no partition, easier to resize).
+
+# Usage
+In archlinux iso session, download this script; then:
+
+```sh
+# without INSTALL=1, help message will be printed.
+INSTALL=1 sh ./install.sh
+```
+
+# NOTE
+- username (sudo-ed) / password / hostname: all are `box`.
+- timezone is set to Asia/Shanghai.
+'
+
 if [ "$INSTALL" != 1 ]; then
-    sed -nE '2,/^[^#]/ s/^# ?(.+|)$/\1/p' "$0"
+    printf "%s" "$HELP"
     exit 0
 fi
 
